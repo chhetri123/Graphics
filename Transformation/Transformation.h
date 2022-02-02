bf1862d414062184d2c547d9bbe2360081cc4be6
chhetri123 <chhetridon123@gmail.com>
2022-02-02 21:45:01 +0545

    collection of all the section(transformations) header file

---
+++ Transformation/Transformation.h
@@ -0,0 +1,10 @@
+#ifndef ROTATE_H
+#define ROTATE_H
+
+void Rotation(float x[],float [],int size,float angle, const std::string& rotation_name="normal");
+void Plot(float  x[],float y[],int size,int color=4);
+void Translation (float  x[],float y[],int size,int t[]);
+void Scaling(float x[], float y[],int size,float sk[],const std::string& scale_from="origin");
+void Shear(float x[],float y[],int size,float ref[],float sh[]);
+
+#endif
